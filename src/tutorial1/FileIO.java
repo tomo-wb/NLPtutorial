22e470df37cd3860abca3343d6146b94f4bdddef
Tomoya Mizumoto <tm0mo.3120@gmail.com>
2014-06-12 12:30:11 +0900

    add directry tutorial2

---
+++ src/tutorial1/FileIO.java
@@ -0,0 +1,95 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TomoyaMizumoto
+ */
+public class FileIO {
+    public FileIO(){
+        
+    }
+    
+    ArrayList<String> FileSTDIN(){
+        ArrayList<String> TextsArray = new ArrayList<>();  // stored texts
+        
+        // input text from stdin
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            TextsArray = new ArrayList<>();
+            String str = br.readLine();
+            while(str != null){
+                TextsArray.add(str);
+                str = br.readLine();
+            }
+            br.close();
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        return TextsArray;
+    }
+    
+    public ArrayList<String> FileReader(String fileName){
+        ArrayList<String> TextsArray = new ArrayList<>();    
+        File file = new File(fileName);
+        
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String str = br.readLine();
+            while(str != null){
+                TextsArray.add(str);
+                str = br.readLine();
+            }
+            br.close();
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        
+        return TextsArray;
+    }
+    
+    void ArrayListPrint(ArrayList<String> arrayList, String filename){
+        try{
+            File file = new File(filename);
+            FileWriter filewriter = new FileWriter(file);
+            for(int i = 0; i < arrayList.size();i++){
+                filewriter.write(arrayList.get(i)+"\n");
+            }
+            filewriter.close();
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+    
+    void StringListPrint(String[] texts, String filename){
+        try{
+            File file = new File(filename);
+            FileWriter filewriter = new FileWriter(file);
+            for(int i = 0; i < texts.length; i++){
+                filewriter.write(texts[i]+"\n");
+            }
+            filewriter.close();
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+    
+    
+    private static void println(Object obj) { System.out.println(obj); }
+}
---
+++ src/tutorial1/TestUnigram.java
@@ -0,0 +1,72 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+/**
+ *
+ * @author TomoyaMizumoto
+ */
+public class TestUnigram {
+    private static final double lambda = 0.95;
+    private static final double lambdaUNK = 1 - lambda;
+    private static double W = 0.0;
+    private static final double V = 1000000;
+    private static double H = 0.0;
+    
+    public static void main(String[] args){
+        //String fileName = "nlp-programming/test/01-test-input.txt";
+        String fileName = "nlp-programming/data/wiki-en-test.word";
+        String modelName = "train.model";
+        FileIO FIO = new FileIO();
+        
+        ArrayList<String> TextsArray = FIO.FileReader(fileName);
+        ArrayList<String> ModelArray = FIO.FileReader(modelName);
+        HashMap<String, Double> model = Array2Hash(ModelArray);
+        evalUnigram(TextsArray, model);
+    }
+    
+    private static void evalUnigram(ArrayList<String> arrayList, HashMap<String, Double> model){
+        int unk = 0;
+        for(int i = 0; i < arrayList.size(); i++){
+            //String text = arrayList.get(i) + " </s>";
+            String[] words = arrayList.get(i).split(" ");
+            ArrayList<String> list = new ArrayList<String> (Arrays.asList(words));
+            list.add("</s>");
+            for(int j = 0; j < list.size(); j++){
+                W += 1;
+                double P = lambdaUNK / V;
+                
+                if(model.containsKey(list.get(j))){
+                    P += lambda * model.get(list.get(j));
+                }
+                else{
+                    unk += 1;   
+                }
+                H  = H - log2(P);
+            }
+        }
+        double entropy = H / W;
+        double coverage = (W-unk) / W;
+        
+        println("entropy = " + entropy);
+        println("coverage = " + coverage);
+    }
+    
+    private static double log2(double p){
+        double log2 = Math.log(p)/Math.log(2.0);
+        return log2;
+    }
+    
+    private static HashMap<String, Double> Array2Hash(ArrayList<String> arrayList){
+        HashMap<String, Double> hashMap = new HashMap<>();
+        for(int i = 0; i < arrayList.size(); i++){
+            String[] wordProb = arrayList.get(i).split("\t");
+            hashMap.put(wordProb[0], Double.parseDouble(wordProb[1]));
+        }
+        return hashMap;
+    }
+    private static void println(Object obj) { System.out.println(obj); }
+}
